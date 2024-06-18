9f9f130ef8ce1f03bb4f5902c1fc76c9ad733426
miriamowowwowo <miriamschulk@gmail.com>
2024-06-18 08:51:47 -0300

    Initial commit

---
+++ Assets/Scripts/PasadorDeObjeto.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PasadorDeObjeto : MonoBehaviour
+{
+    public GameObject[] objetos;
+    public int currentIndex = -1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DeactivateAll();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            
+            currentIndex++;
+            if (currentIndex < objetos.Length)
+            {
+                ActivateByIndex(currentIndex);
+            }
+            if (currentIndex > objetos.Length - 1)
+            {
+                currentIndex = -1;
+                DeactivateAll();
+            }
+        }
+    }
+
+    void DeactivateAll()
+    {
+        for (int i = 0; i < objetos.Length; i++)
+        {
+            objetos[i].SetActive(false);
+        }
+    }
+
+    void ActivateByIndex(int index)
+    {
+        objetos[index].SetActive(true);
+    }
+
+}
+
